6147bc53cd9918270cfccb100b03b7ea49bb07e4
ivancaminal <ivancaminal72@gmail.com>
2020-02-24 20:28:29 +0100

    kitty download definitive

--- download/kitti_odometry_dw.sh
+++ download/kitti_odometry_dw.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-set -e 
-cd ~/datasets && rm -rf ./kitti/ && mkdir kitti && cd kitti
-links=(
-"https://s3.eu-central-1.amazonaws.com/avg-kitti/devkit_odometry.zip"
-"https://s3.eu-central-1.amazonaws.com/avg-kitti/data_odometry_poses.zip"
-"https://s3.eu-central-1.amazonaws.com/avg-kitti/data_odometry_calib.zip"
-"https://s3.eu-central-1.amazonaws.com/avg-kitti/data_odometry_gray.zip"
-"https://s3.eu-central-1.amazonaws.com/avg-kitti/data_odometry_color.zip"
-"https://s3.eu-central-1.amazonaws.com/avg-kitti/data_odometry_velodyne.zip"
+set -e
+rm -rf ~/datasets/kitti/logs_download/* && mkdir -p ~/datasets/kitti/logs_download && cd ~/datasets/kitti
+files=(
+"devkit_odometry"
+"data_odometry_poses"
+"data_odometry_calib"
+"data_odometry_gray"
+"data_odometry_color"
+"data_odometry_velodyne"
 )
 
-for idx in ${!links[@]}; do
-	echo "Downloading link: $idx"
-	wget -O $idx.zip ${links[$idx]} > log_$idx.txt 2>&1 && unzip -q $idx.zip && rm $idx.zip &
+for file in ${files[@]}; do
+	rm -rf ~/datasets/kitti/${file}/*
+	echo "Downloading: ${file}"
+	wget -O ${file}.zip "https://s3.eu-central-1.amazonaws.com/avg-kitti/${file}.zip" > ./logs_download/dw_${file}.txt 2>&1 && mkdir ${file} && unzip -o -d "./${file}/" ${file}.zip > ./logs_download/xz_${file}.txt 2>&1  && rm -f ${file}.zip &
 done
