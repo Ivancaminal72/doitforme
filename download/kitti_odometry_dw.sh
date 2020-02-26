3846621bc311195eef0518e8d18313e83d5df76a
ivancaminal <ivancaminal72@gmail.com>
2020-02-26 19:11:43 +0100

    reorganize dirs kitti dw & script improvements

--- download/kitti_odometry_dw.sh
+++ download/kitti_odometry_dw.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -e
-rm -rf ~/datasets/kitti/logs_download/* && mkdir -p ~/datasets/kitti/logs_download && cd ~/datasets/kitti
+
+if [[ $1 == --h* ]] || [[ $1 == "" ]]; then
+	echo -e "Usage: $0 /path/to/datasets (creates kitti dir)"; exit; fi;
+if [[ ! -d $1 ]]; then echo "Invalid $1"; exit 1; fi;
+if [[ ! -w $1 ]]; then echo "User doesn't have write persmision to $1"; exit 1; fi;
+dataset_dir="${1%/}/kitti"
+if [[ -d $dataset_dir ]]; then echo "Already exists $dataset_dir (delete it)"; exit 1; fi;
+mkdir -p $dataset_dir/logs_download && cd $dataset_dir
 files=(
 "devkit_odometry"
 "data_odometry_poses"
@@ -11,7 +18,27 @@ files=(
 )
 
 for file in ${files[@]}; do
-	rm -rf ~/datasets/kitti/${file}/*
+	rm -rf $dataset_dir/${file}/*
 	echo "Downloading: ${file}"
 	wget -O ${file}.zip "https://s3.eu-central-1.amazonaws.com/avg-kitti/${file}.zip" > ./logs_download/dw_${file}.txt 2>&1 && mkdir ${file} && unzip -o -d "./${file}/" ${file}.zip > ./logs_download/xz_${file}.txt 2>&1  && rm -f ${file}.zip &
 done
+
+wait #until download jobs finish
+echo "Reorganizing dirs structure"
+mv devkit_odometry/devkit/ .
+mv data_odometry_poses/dataset/poses/ .
+rsync -av --remove-source-files ./data_odometry_velodyne/dataset/sequences/ ./data_odometry_color/dataset/sequences/ > ./logs_download/rg_velodyne-color.txt 2>&1 &
+rsync -av --remove-source-files ./data_odometry_gray/dataset/sequences/ ./data_odometry_color/dataset/sequences/ > ./logs_download/rg_gray-color.txt 2>&1 &
+rsync -av --remove-source-files ./data_odometry_calib/dataset/sequences/ ./data_odometry_color/dataset/sequences/ > ./logs_download/rg_calib-color.txt 2>&1 &
+wait
+mv data_odometry_color/dataset/sequences/ .
+rm -rf devkit_odometry/
+rm -rf data_odometry_poses/
+rm -rf data_odometry_calib/
+rm -rf data_odometry_gray/
+rm -rf data_odometry_color/
+rm -rf data_odometry_velodyne/
+echo "OK!"
+
+
+
--- evaluate/ate_rtabmap_kitti_worker.sh
+++ evaluate/ate_rtabmap_kitti_worker.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 #PARAMETERS
-if [[ $1 == "-h" ]] || [[ $1 == "--help" ]]; then echo -e "Usage: $0 /path/to/experiments/output/directory"; exit; fi;
+if [[ $1 == --h* ]] || [[ $1 == "" ]]; then 
+    echo -e "Usage: $0 /path/to/experiments/output/directory"; exit; fi;
 out_dir=${1%/}
-if [ ! -d "$out_dir" ]; then echo "ERROR: Invalid experiments output directory: $out_dir"; exit -1; fi
+if [ ! -d "$out_dir" ]; then echo "ERROR: Invalid experiments output directory: $out_dir"; exit 1; fi
 IFS='/'; read -r -a ordir <<< $out_dir; unset IFS; #Parse dirnames "~/outputs/phd/kitti/rtab/unscaled"
 group=${ordir[-1]}
 system=${ordir[-2]}
