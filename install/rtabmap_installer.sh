91872ef9fef929e017e6015d7548f780aa775ecb
ivancaminal <ivancaminal72@gmail.com>
2020-02-26 20:05:04 +0100

    rtab017melodic installation

--- install/rtabmap_installer.sh
+++ install/rtabmap_installer.sh
@@ -10,11 +10,12 @@ distro=melodic
 cd $HOME/workspace/phd/rtabmap/
 git clone https://github.com/introlab/${project}.git
 mv ${project}/ ${project}_${distro}_${version}/
-mv ${project}/.git/ ${project}/.git2/ #multirepo w/ possible future modifications 
+cd ${project}_${distro}_${version}/
+git checkout release/${distro}/rtabmap/${version}
+mv .git/ .git2/ #deactivate (multirepo w/ possible future modifications)
 
 #Remote build (after local upload)
-# cd ${project}_${distro}_${version}/
-# git checkout release/${distro}/rtabmap/${version}
+# cd $HOME/workspace/phd/rtabmap/${project}_${distro}_${version}/
 # mkdir build && cd build
 # cmake .. -DCMAKE_INSTALL_PREFIX=$HOME/local/${project}_${distro}_${version} -DWITH_FREENECT=OFF -DWITH_FREENECT2=OFF -DWITH_QT=OFF -DBUILD_TOOLS=OFF -DBUILD_EXAMPLES=OFF
 # srun -p gpi.develop --mem 20G -c12 make -j12 #gpic03 - OK
