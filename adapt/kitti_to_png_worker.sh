e051551d202205fa40da5bd56dd3b8f9d59aa971
ivancaminal <ivancaminal72@gmail.com>
2020-02-27 19:57:31 +0100

    kitti_to_png small fix time resetdir

--- adapt/kitti_to_png_worker.sh
+++ adapt/kitti_to_png_worker.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 seq=0
 downsampling=1
-while [ "$seq" -lt 22 ]
+while [ "$seq" -lt 11 ]
 do
     printf -v seq_f "%02d" $seq
     echo "Adapting sequence $seq_f"
-	srun -p gpi.develop --time=0:30:00 -c1 --mem=8G $HOME/workspace/phd/adapt/kitti/kitti_to_png/build/kitti_to_png -r 120 -d $downsampling -i $HOME/datasets/kitti/ -o $HOME/datasets/kitti/generated/ -s $seq_f
+	srun -p gpi.compute --time=10:00:00 -c1 --mem=8G $HOME/workspace/phd/adapt/kitti/kitti_to_png/build/kitti_to_png -r 120 -d $downsampling -i $HOME/datasets/kitti/ -o $HOME/datasets/kitti/generated/ -s $seq_f
     (( seq++ ))
 done
 echo all done!
