60e934d0745b6858f43b2a27c020ad6ff1feed91
ivancaminal <ivancaminal72@gmail.com>
2020-03-03 11:30:34 +0100

    GT 2 freiburg (bring back)

---
+++ adapt/groundtruth_kitti_to_freiburg_worker.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#Execution time (10 seq) ~7h
+seq=0
+while [ "$seq" -lt 11 ]
+do
+    printf -v seq_f "%02d" $seq
+    echo "Converting groundtruth $seq_f"
+    srun -p gpi.compute --time=1:00:00 -c1 --mem=8G $HOME/workspace/phd/adapt/kitti/groundtruth_kitti_to_freiburg/build/groundtruth_kitti_to_freiburg -i ~/datasets/kitti/poses/$seq_f.txt -o ~/datasets/kitti/poses/${seq_f}_freiburg.txt -t ~/datasets/kitti/sequences/$seq_f/times.txt
+    (( seq++ ))
+done
+echo all done!
