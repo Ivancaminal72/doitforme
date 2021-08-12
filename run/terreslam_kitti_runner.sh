aeb3b6a8d78e815ad8dfc7dfcf7282397a2b14b8
Ivan Caminal <ivancaminal72@gmail.com>
2021-08-12 13:54:41 +0200

    update terreslam kitti runner

--- run/terreslam_kitti_runner.sh
+++ run/terreslam_kitti_runner.sh
@@ -3,8 +3,10 @@ rosparam load ~/workspace/ros_ddd/src/terreslam/config.yaml
 rosrun terreslam rgb_depth_frontend &
 rosrun terreslam plane_detector_frontend &
 rosrun terreslam blob_detector_frontend &
+rosrun terreslam metric_alignment_frontend &
 # sleep 3
 # rosrun data_to_rosbag pcd_to_png -p 2 -r 0.1 &
 # sleep 7
 # rosrun data_to_rosbag kitti_live_node -s ~/datasetkitti/sequences/04 -r 0.1
-rosrun rviz rviz -d ~/workspace/ros_ddd/rviz/basic.rviz
+
+# rosrun rviz rviz -d ~/workspace/ros_ddd/rviz/basic.rviz
